6b2abc19e1f088ca932d3138c8dae53330c22539
MarcoB <mrc.bsllt@gmail.com>
2021-01-21 14:02:55 +0100

    primo commit test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  $prova = "ciao"
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <link rel="stylesheet" href="css/style.css">
+     <title>Google faq</title>
+   </head>
+   <body>
+   </body>
+ </html>
